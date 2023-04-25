5dcf87bdec0353a8869a0d11feb13c055aedbe5c
Qiang Sun <sunqiang891103@gmail.com>
2023-04-25 17:26:33 +0800

    add param for print_hello

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -2,7 +2,7 @@
  * @file: 
  * @Author: Qiang Sun
  * @Date: 2023-04-25 13:31:47
- * @LastEditTime: 2023-04-25 13:55:36
+ * @LastEditTime: 2023-04-25 17:24:15
  * @version: 
  * @copyright: ROSIWIT Copyright (c) 2023
  * @brief: 
@@ -10,13 +10,13 @@
 #include <iostream>
 #include <thread>
 
-void print_hello() {
-  std::cout << "Hello from thread!" << std::endl;
+void print_hello(std::string name) {
+  std::cout << "Hello " << name << std::endl;
 }
 
 int main() {
   // 创建一个新的线程，执行print_hello函数
-  std::thread t(print_hello);
+  std::thread t(print_hello,"Jack");
 
   // 在主线程中打印一条消息
   std::cout << "Hello from main thread!" << std::endl;
